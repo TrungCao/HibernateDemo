17ff956da9a78bc3ccbc67a48a166bacd4ff643a
TrungCao <caotrung912196@gmail.com>
2022-12-14 23:00:43 +0700

    add employeeService.java and FresherService.java

---
+++ src/main/java/vn/trung/app/EmployeeService.java
@@ -0,0 +1,5 @@
+package vn.trung.app;
+
+public class EmployeeService {
+
+}
---
+++ src/main/java/vn/trung/app/FresherService.java
@@ -0,0 +1,13 @@
+package vn.trung.app;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class FresherService {
+    private String name;
+    private Long age;
+    private String address;
+
+}
